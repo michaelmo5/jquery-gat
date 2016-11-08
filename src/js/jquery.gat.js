fba7f8f9a4d2335a4065fd7abb0fd69fbfb60b18
Michał Moskal <michal.moskal.55@gmail.com>
2016-11-09 00:24:17 +0100

    + plugin wrap and controls css

--- src/js/jquery.gat.js
+++ src/js/jquery.gat.js
@@ -1,3 +1,26 @@
-$(document).ready(function(){
-	//
-});
+(function( $ ) {
+	$.fn.gat = function( options  ) {
+		
+		var settings = $.extend({
+			GOOGLE_SPEECH_API_KEY: "YOUR_API_KEY",
+			language: "en"
+		}, options );
+		
+		this.addClass( "gat-textarea" );
+		this.wrap('<div class="gat-widget">').after('<div class="gat-controls"> </div>');
+		
+		
+		
+		return this;
+	};
+	
+	$(document).on("click", ".gat-controls", function(){
+		if(!$(this).hasClass('active')){
+			// Start Recording
+			$(this).addClass('active');
+		} else {
+			// Stop & Process
+			$(this).removeClass('active');
+		}
+	});
+}( jQuery ));
